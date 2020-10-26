16e8f67dc505fb1f862db551285b5db78d64ae34
Adam Thorne <ahthornev2@gmail.com>
2020-10-26 23:12:07 +0000

    Created an assertion function to test code that we write in the future

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("🚩🚩🚩 Assertion Failed: " + actual + " !== " + expected);
+  } else if (actual === expected) {
+    console.log("🍀🍀🍀 Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
