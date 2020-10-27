b6cc93b2520d76c8ad32cbc8e1dea8ae56d9d449
Adam Thorne <ahthornev2@gmail.com>
2020-10-27 07:41:29 +0000

    Refactored tail function

--- tail.js
+++ tail.js
@@ -7,11 +7,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(array) {
-  let tailArray = [];
-  for (let i = 1; i < array.length; i++) {
-    tailArray.push(array[i]);
-  }
-  return tailArray;
+  return array.slice(1);
 };
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
@@ -21,7 +17,6 @@ assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
-
 
 const empty = [];
 console.log(tail(empty));
