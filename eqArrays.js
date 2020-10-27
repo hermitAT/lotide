f07694d065c8a7da20f5387a7ad6969e849fa7c8
Adam Thorne <ahthornev2@gmail.com>
2020-10-27 22:27:41 +0000

    create a function to loop through arrays to determine if the contents (and as such entire array) is equal, using type strict comparison operators!

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`🍀🍀🍀 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
