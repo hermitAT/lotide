bbc56be79c9aec20c30a075d02a7dd3515febe92
Adam Thorne <ahthornev2@gmail.com>
2020-10-28 02:21:48 +0000

    create a function to 'flatten' an array, checking if elements within it are also arrays and sending them all into one new array

---
+++ flatten.js
@@ -0,0 +1,35 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const flatten = function(array) {
+  let flatArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        flatArray.push(array[i][j]);
+        console.log(flatArray);
+      }
+    } else {
+      flatArray.push(array[i]);
+    }
+  } return flatArray;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
