fcdfcdeb640f0b3d4d8a6257bd35ac5add6ad00d
Adam Thorne <ahthornev2@gmail.com>
2020-10-28 22:16:55 +0000

    re-commiting after a mentor-reviewed refactor

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`🍀🍀🍀 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  for (const key in obj) {
+    if (obj[key] === value) {
+      return obj[key];
+    }
+  }
+};
+
+
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
--- letterPositions.js
+++ letterPositions.js
@@ -22,17 +22,16 @@ const letterPositions = function(string) {
   
   for (let i = 0; i < string.length; i++) {
     const letter = string[i];
-    const letterIdxPos = string.indexOf(letter, i);
     
     if (letter !== ' ') {
       // if the property doesn't exist yet, create it and create an array
       // containing the index
       if (!Array.isArray(letterIdxLibrary[letter])) {
-        letterIdxLibrary[letter] = [letterIdxPos];
+        letterIdxLibrary[letter] = [i];
       } else {
         // if letterIdxLibrary contains a property with the same name as the letter,
         // push the index that the letter is at to an array within that property.
-        letterIdxLibrary[letter].push(letterIdxPos);
+        letterIdxLibrary[letter].push(i);
       }
     }
   }
