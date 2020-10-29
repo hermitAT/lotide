30941006aa30c75731f66213a87af796a287fdac
Adam Thorne <ahthornev2@gmail.com>
2020-10-29 02:22:19 +0000

    Finishing touches on functionality for comparing key values within two objects

--- eqObjects.js
+++ eqObjects.js
@@ -6,14 +6,35 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 
 const eqObjects = function(object1, object2) {
-  const numOfKeys1 = Object.keys(object1).length;
-  const numOfKeys2 = Object.keys(object2).length;
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
 
-  console.log(numOfKeys1, numOfKeys2);
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  } else {
+    for (const key of object1Keys) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        return eqArrays(object1[key], object2[key]);
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  } return true;
 };
 
 const ab = { a: "1", b: "2" };
@@ -23,5 +44,14 @@ eqObjects(ab, ba); // => true
 const abc = { a: "1", b: "2", c: "3" };
 eqObjects(ab, abc); // => false
 
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
+
 assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), false);
+assertEqual(eqObjects(cd, dc), true);
+assertEqual(eqObjects(cd, cd2), false);
