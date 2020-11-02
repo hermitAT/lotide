1dd9dd08f07955d83a71b3e0e598bd400a77e7fc
Adam Thorne <ahthornev2@gmail.com>
2020-11-02 01:58:20 +0000

    implement recursion to loop through nested arrays and determine their equality

--- eqArrays.js
+++ eqArrays.js
@@ -10,17 +10,19 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
+  
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
       return false;
     }
   } return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
